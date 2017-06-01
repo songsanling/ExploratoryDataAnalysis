1c8b24492380318de491ff5cb73c286c32662e60
songsanling <lamusesi38sanling@gmail.com>
2017-05-31 23:54:04 -0700

    Create plot5.R

---
+++ Assignment2/plot5.R
@@ -0,0 +1,13 @@
+# subsetting SCC with vehicle values
+vehicleMatches  <- grepl("vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+subsetSCC <- SCC[vehicleMatches, ]
+
+# merging dataframes
+NEISCC <- merge(balt, subsetSCC, by="SCC")
+
+# summing emission data per year per type
+totalEmissions <- tapply(NEISCC$Emissions, NEISCC$year, sum)
+
+# plotting
+barplot(totalEmissions, xlab = "Year", ylab = "Total Emission (ton)", 
+        main = "Total Emission from motor sources in Baltimore")
